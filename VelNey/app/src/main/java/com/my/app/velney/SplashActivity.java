eb4e1a34e0e1fb08f81c7ad13fee6420e99a0258
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-04-26 00:33:16 +0700

    26_4_2021 update

--- VelNey/app/src/main/java/com/my/app/velney/SplashActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/SplashActivity.java
@@ -2,29 +2,35 @@ package com.my.app.velney;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    ImageView applogo:
+    ImageView applogo;
+
     @Override
     protected void OnCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         //mendapati ID pada XML
         applogo = findViewById(R.id.applogo);
 
-        //Memberikan event ke applogo
-        applogo.setOnClickListener(new.View.OnClickListener() {
+        //membuat timer untuk pindah activity secara otomatis
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() { //delay proses ini di jalankan
             @Override
-            public void onClick(View v) {
+            public void run() {
+                //Melakukan sesuati setelah 2s
                 //menuju halman lain
                 Intent abc = new Intent(package context:SplashActivity.this, GetStartedActivity.class);
                 startActivity(abc);
                 finish();
             }
-        });
+        }, 2000);
+
     }
 }
