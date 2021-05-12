8902f481e271381058b9f67da0c6a6fecfb1e119
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-05-12 20:06:48 +0700

    up_12_5_21: Get Started Page and Splash Screen Interface Finished :)

--- VelNey/app/src/main/java/com/my/app/velney/SplashActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/SplashActivity.java
@@ -16,5 +16,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //set timer for 1 second
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Change activity to another activity
+                Intent gogetstarted = new Intent(SplashActivity,GetStartedActivity.class);
+                startActivity(gogetstarted);
+                finish();
+            }
+        }, 1000);// 1000 ms = 1 s
     }
 }
