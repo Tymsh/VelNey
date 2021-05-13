11e0a7840c28c38a56c79a35b89f56750b925a8f
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-05-13 22:34:28 +0700

    up_13_5_21_registration_page

--- VelNey/app/src/main/java/com/my/app/velney/GetStartedActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/GetStartedActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class GetStartedActivity extends AppCompatActivity {
 
-    Button btn_sign_in;
+    Button btn_sign_in, btn_new_account_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,7 @@ public class GetStartedActivity extends AppCompatActivity {
         setContentView(R.layout.activiry_get_started);
 
         btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_new_account_create = findViewById(R.id.btn_new_account_create);
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +27,14 @@ public class GetStartedActivity extends AppCompatActivity {
                 startActivity(gotosignin);
             }
         });
+
+        btn_new_account_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoregisterone = new Intent(GetStartedActivity.this, RegisterOneActivity.class);
+                startActivity(gotoregisterone);
+            }
+        });
+
     }
 }
