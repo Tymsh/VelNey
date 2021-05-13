ed4ac1add094c8cdff62f054d582e696ff9af8ed
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-05-13 19:03:17 +0700

    up_13_5_21_get_started_page

--- VelNey/app/src/main/java/com/my/app/velney/GetStartedActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/GetStartedActivity.java
@@ -1,15 +1,30 @@
 package com.my.app.velney;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    Button btn_sign_in;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiry_get_started);
 
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosignin = new Intent(GetStartedActivity.this, SignInActivity.class);
+                startActivity(gotosignin);
+            }
+        });
     }
 }
