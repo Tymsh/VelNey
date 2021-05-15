a59bbb029dc47ce47a4f2698a3c44e2a9a7793c4
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-05-15 23:20:51 +0700

    up_15_5_21_register_one_act
    still have error on setOnClickListener

--- VelNey/app/src/main/java/com/my/app/velney/RegisterOneActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/RegisterOneActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterOneActivity extends AppCompatActivity {
 
+    Button btn_continue;
     LinearLayout btn_back;
 
     @Override
@@ -16,14 +18,22 @@ public class RegisterOneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_one);
 
-        btn_back = findViewById(R.id.btn_back);
+        btn_continue = findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent gotonextregister = new Intent(RegisterOneActivity.this,RegisterTwoActivity.class);
+                startActivity(gotonextregister);
+            }
+        });
+        }
 
+        btn_back = findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent backtosignin = new Intent(RegisterOneActivity.this, SignInActivity.class);
                 startActivity(backtosignin);
-            }
-        });
-    }
+        }
+    });
 }
