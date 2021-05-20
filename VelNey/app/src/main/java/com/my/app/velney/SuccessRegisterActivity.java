495d39c684b711aff17be1c61d044dd21251e24a
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-05-20 23:59:22 +0700

    up_20_5_21_4

--- VelNey/app/src/main/java/com/my/app/velney/SuccessRegisterActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/SuccessRegisterActivity.java
@@ -2,13 +2,27 @@ package com.my.app.velney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccessRegisterActivity extends AppCompatActivity {
+
+    Button btn_explore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_register);
+
+        btn_explore = findViewById(R.id.btn_explore);
+        btn_explore.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent gotohome = new Intent(SuccessRegisterActivity.this, HomeActivity.class);
+                startActivity(gotohome);
+            }
+        });
     }
 }
