f83098f889ab28914c1305d1f99da6f89ce93e77
ymichellesh <65338274+Tymsh@users.noreply.github.com>
2021-06-08 23:21:07 +0700

    Up_8_6_21
    eror :(

--- VelNey/app/src/main/java/com/my/app/velney/HomeActivity.java
+++ VelNey/app/src/main/java/com/my/app/velney/HomeActivity.java
@@ -14,14 +14,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tourist_1= findViewById(R.id.tourist_1);
+        tourist_2 = findViewById(R.id.tourist_2);
+        borobudur = findViewById(R.id.borobudur);
+        monas =  findViewById(R.id.monas);
+        pagoda = findViewById(R.id.pagoda);
+        pisa = findViewById(R.id.pisa);
+        sphinx = findViewById(R.id.sphinx);
+        tori = findViewById(R.id.tori);
+        ticket_2 = findViewById(R.id.ticket_2);
     }
 
-    tourist_1 = findViewById(R.id.tourist_1);
-    tourist_2 = findViewById(R.id.tourist_2);
-    borobudur = findViewById(R.id.borobudur);
-    monas =  findViewById(R.id.monas);
-    pagoda = findViewById(R.id.pagoda);
-    pisa = findViewById(R.id.pisa);
-    sphinx = findViewById(R.id.sphinx);
-    tori = findViewById(R.id.tori);
-    ticket_2 = findViewById(R.id.ticket_2);
+
